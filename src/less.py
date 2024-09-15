f330175a0745090119b585bc7a6720dab7c90513
timm <timm@ieee.org>
2024-09-15 12:18:02 -0400

    kneans experiments

--- src/less.py
+++ src/less.py
@@ -31,6 +31,8 @@ def NUM(c=0, x=" "): return o(This=NUM, c=c, txt=x, n=0,
                               mu=0, m2=0, sd=0, lo=1E32, hi=-1E32, 
                               goal = 0 if x[-1]=="-" else 1)
 
+def CLUSTER(mid,data): return o(This=CLUSTER, data=data, mid=mid, nearest=1E32, close=None)
+
 def sym(i,v): 
   i.n += 1
   i.has[v] = 1 + i.has.get(v,0)
@@ -42,33 +44,36 @@ def num(i,v):
   d = v - i.mu
   i.mu += d / i.n
   i.m2 += d * (v - i.mu)
-  i.sd  = 0 if i.n < 2 else (i.m2 / (i.n - 1))**0.5
+  i.sd = 0 if i.n < 2 else (i.m2/(i.n - 1))**.5 
 
 def data(i,row):
-  def goalp(v): return v[-1] in "+-!"
-  def nump(v):  return v[0].isupper()
+  def goalp(v):  return v[-1] in "+-!"
+  def nump(v):   return v[0].isupper()
+  def ignorep(v): return v[-1] == "X"
   def head(c,v):
     col = (NUM if nump(v) else SYM)(c,v)
-    (i.cols.nums if nump(v)  else i.cols.syms).append(col)
-    (i.cols.y    if goalp(v) else i.cols.x   ).append(col)
     i.cols.all.append(col)
+    if not ignorep(v):
+      (i.cols.y    if goalp(v) else i.cols.x   ).append(col)
   if i.cols.all == []:
     i.cols.names = row
     [head(c,v) for c,v in enumerate(row)]
   else:
     i.rows += [row]
-    [sym(col, row[col.c]) for col in i.cols.syms if row[col.c] != "?"]
-    [num(col, row[col.c]) for col in i.cols.nums if row[col.c] != "?"]
+    for cols in [i.cols.x, i.cols.y]:
+      for col in cols:
+        v = row[col.c]
+        if v != "?":
+          (sym if col.This is SYM else num)(col, v)
   return i
     
 def datas(i,src): [data(i,row) for row in src]; return i
 def mids(data)  : return [mid(col) for col in data.cols.all]
 def mid(i)      : return i.mu if i.This is NUM else max(i.has,key=i.has.get)
-def div(i)      : return i.sd if i.This is NUM else entropy(i.has)
 def norm(i,x)   : return x if x=="?" else (x - i.lo) / (i.hi - i.lo + 1E-32)
 
-def clone(i):
-  return data(DATA(), i.cols.names)
+def clone(i,rows=[]):
+  return datas(data(DATA(), i.cols.names),rows)
 
 def xDist(data, row1, row2):
   def dist(x,a,b):
@@ -80,26 +85,23 @@ def xDist(data, row1, row2):
     return abs(a - b)
   d = sum(dist(x, row1[x.c], row2[x.c])**the.p for x in data.cols.x)
   return d**(1/the.p) / len(data.cols.x)**(1/the.p)
-  
+
 def yDist(data, row):
  return max(abs(y.goal - norm(y,row[y.c])) for y in data.cols.y)
 
 def kmeans(data1, k=16, loops=10, samples=512):
+  samples = min(len(data1.rows),samples)
   rows = random.choices(data1.rows, k=samples)
   def loop(loops, centroids):
     d = {}
     for row in rows:
-      k    = id(min(centroids, key=lambda r: xDist(data1,r,row)))
+      k = id(min(centroids, key=lambda r: xDist(data1,r,row)))
       d[k] = d.get(k,None) or clone(data1)
-      data(d[k], row)
+      data(d[k],row)
     return loop(loops-1, [mids(data2) for data2 in d.values()]) if loops else d.values()
   return loop(loops, rows[:k])
 
 #-----------------------------------------------------------------------
-def entropy(d):
-  N = sum(n for n in d.values())
-  return - sum(n/N * log(n/N,2) for n in d.values())
-
 def pretty(x):
   if isinstance(x,float)   : return f"{x:.3f}"
   if isinstance(x,list )   : return "["+', '.join([pretty(y) for y in x])+"]"
@@ -115,7 +117,8 @@ def csv(file):
   with file_or_stdin(None if file=="−" else file) as src: 
     for line in src: 
       line = re.sub(r"([\n\t\r ]|\#.*)", "", line)
-      if line: yield [coerce(s.strip()) for s in line.split(",")]
+      if line: 
+        yield [coerce(s.strip()) for s in line.split(",")]
 
 def cli(d):
   for k,v in d.items():
@@ -151,14 +154,23 @@ class eg:
     print(pretty(sorted([yDist(d, mids(x)) for x in kmeans(d)])))
 
   def kmeans2(_): 
-    d    = datas(DATA(),csv(the.train))
-    fun  = lambda d1:yDist(d, mids(d1))
-    rows = sorted(kmeans(d,k=12,samples=512), key=fun)[0].rows
-    rows = sorted(kmeans(datas(clone(d),rows),k=4,samples=len(rows)),key=fun)[0].rows
-    for row in rows: print(row,yDist(d,row))
+    d         = datas(DATA(),csv(the.train))
+    n=NUM()
+    [num(n,yDist(d,x)) for x in d.rows]
+    print(f"mu= {n.mu:.3f}, 'zero'= {n.lo+0.35*n.sd:.3f}")
+    fun       = lambda d1:yDist(d, mids(d1))
+    k1=12; k2=6; k3=6
+    for _ in range(20):
+      clusters1 = kmeans(d, k=k1)
+      rows1     = sorted(clusters1, key=fun)[0].rows
+      clusters2 = kmeans(clone(d,rows1),k=k2)
+      rows2     = sorted(clusters2, key=fun)[0].rows
+      random.shuffle(rows2)
+      row       = sorted(rows2[:k3], key=lambda r:yDist(d,r))[0]
+      print(f"{yDist(d,row):.3f}",k1+k2+k3,row,sep="\t")
 
 #-----------------------------------------------------------------------
-cli(the.__dict__)
 random.seed(the.seed)
+cli(the.__dict__)
 for i,s in enumerate(sys.argv):
   getattr(eg,s[1:], lambda _:_)(i+1)
