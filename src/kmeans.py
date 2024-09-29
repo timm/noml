0d3b59ab291fc1043743d67589946a1a0590f4e6
timm <timm@ieee.org>
2024-09-29 10:00:49 -0400

    adding back twoFars

---
+++ src/kmeans.py
@@ -0,0 +1,26 @@
+def kmeans(data1, k=10, loops=10, samples=512):
+  def loop(n, centroids):
+    datas = {}
+    for row in rows:
+      k = id(min(centroids, key=lambda centroid: xdist(data1,centroid,row)))
+      datas[k] = datas.get(k,None) or DATA(data1.cols.names)
+      data(datas[k], row)
+    return datas.values() if n==0 else loop(n-1, [mid(d) for d in datas.values()])
+
+  random.shuffle(data1.rows)
+  rows = data1.rows[:samples]
+  return loop(loops, rows[:k])
+
+def diversity(data1,samples=512):
+  n = the.Samples
+  clone = lambda a: DATA(data1.cols.names,a)
+  rows  = shuffle(data1.rows)[:samples]
+  done  = []
+  for k in [n,n]:
+    datas = kmeans(clone(rows), k=k)
+    done += [mid(d) for d in datas]
+    data2 = clone(done) 
+    rows  = sorted(datas, key=lambda d: ydist(data2, mid(d)))[0].rows
+  return ydists(clone(done + shuffle(rows)[:n*2])).rows[0]
+
+
